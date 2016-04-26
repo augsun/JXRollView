d1cacec48caa33fcf99c89b8351a184a42b111eb
CoderSun <shiba_iosJX@shiba-iosjxs-mbp.local>
2016-04-26 16:35:55 +0800

    no

--- JXRollView/ViewController.m
+++ JXRollView/ViewController.m
@@ -27,24 +27,29 @@
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
     self.view.backgroundColor = [UIColor colorWithRed:239/255.f green:239/255.f blue:244/255.f alpha:1.f];
-    
-#define STR_CAT(str1, str2)     [NSString stringWithFormat:@"%@%@", str1, str2]
-    NSString *strUrlBase = @"http://img.51shiba.com/";
+#define STR_CAT2(str1, str2)    [NSString stringWithFormat:@"%@%@", str1, str2]
+#define URL_FROM_NAME(name)     [NSURL URLWithString:STR_CAT2(strUrlBase, STR_CAT2(name, STR_CAT2(strWScreen, @".jpg")))]
+    NSString *strWScreen = [NSString stringWithFormat:@".%ld", (long)[UIScreen mainScreen].bounds.size.width];
+    NSString *strUrlBase = @"https://raw.githubusercontent.com/augsun/Resources/master/JXRollView/";
     NSArray <NSURL *> *arrUrls = @[
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/02/FE/Cvmki1b80S-AcaM0AAEnegwfb4Y261.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/03/46/Cvmki1cLVTWASqItAACZKcsjVhY117.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/02/FF/Cvmki1b80W2ARBqqAAFQE2bkZ-Q237.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/02/FF/Cvmki1b80WyASlfhAAES1pEwsjI653.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/03/39/Cvmki1cHUXeAPQn6AACgVAcdtk0325.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/03/39/Cvmki1cHLd-AEMkUAADiBAlh4VU041.jpg")],
-                                   [NSURL URLWithString:STR_CAT(strUrlBase, @"M00/03/38/Cvmki1cHJiCASqhdAACZreZxFCw557.jpg")],
+                                   URL_FROM_NAME(@"101749"),
+                                   URL_FROM_NAME(@"107500"),
+                                   URL_FROM_NAME(@"108389"),
+                                   URL_FROM_NAME(@"111258"),
+                                   URL_FROM_NAME(@"112793"),
+                                   URL_FROM_NAME(@"355362"),
+                                   URL_FROM_NAME(@"931282"),
+                                   URL_FROM_NAME(@"969118"),
+                                   URL_FROM_NAME(@"970400"),
+                                   URL_FROM_NAME(@"986765"),
                                    ];
-
 #undef STR_CAT
-    CGFloat yLocation = 60;
-    CGFloat imgRate = 2.f;
+#undef URL_FROM_NAME
+    CGFloat yLocation = 20;
+    CGFloat imgRate = 16 / 9.f;
     CGFloat wScreen = [UIScreen mainScreen].bounds.size.width;
     
+// ==========================================================================================
 #pragma mark 以创建 pageControl 为自定义图片 的 JXRollView 为例
     // STEP 1 创建 JXRollView
     __weak __typeof(self) weakSelf = self;
@@ -73,13 +78,12 @@
      * (详见 demo)2. 在 AppDelegate applicationDidEnterBackground 和 applicationWillEnterForeground 发送相应通知;
      */
     
-    
 #pragma mark 创建 pageControl 为自定义颜色 的 JXRollView
-    CGFloat spa_Edge = 30;
-    CGRect rectRollViewAnotherKind = CGRectMake(spa_Edge,
+    CGFloat spa_Edge = 8;
+    CGRect rectRollViewAnotherKind = CGRectMake(2 * spa_Edge,
                                                 yLocation + _jxRollView.frame.size.height + spa_Edge,
-                                                wScreen - 2 * spa_Edge,
-                                                120);
+                                                wScreen - 4 * spa_Edge,
+                                                (wScreen - 2 * spa_Edge) / imgRate );
     _jxRollViewAnotherKind = [[JXRollView alloc] initWithFrame:rectRollViewAnotherKind
                                           indicatorColorNormal:nil
                                        indicatorColorHighlight:nil
@@ -91,10 +95,10 @@
     [_jxRollViewAnotherKind jx_refreshRollViewByUrls:[arrUrls subarrayWithRange:NSMakeRange(1, 5)]];
     
     //
-    CGRect rectRollViewLast = CGRectMake(spa_Edge,
+    CGRect rectRollViewLast = CGRectMake(2 * spa_Edge,
                                          _jxRollViewAnotherKind.frame.origin.y + _jxRollViewAnotherKind.frame.size.height + spa_Edge,
-                                         wScreen - 2 * spa_Edge,
-                                         120);
+                                         wScreen - 4 * spa_Edge,
+                                         (wScreen - 2 * spa_Edge) / imgRate);
 
     _jxRollViewLast = [[JXRollView alloc] initWithFrame:rectRollViewLast
                                    indicatorColorNormal:[UIColor lightGrayColor]
